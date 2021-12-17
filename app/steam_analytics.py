18deda372ff7823b30357353b89aa54e7d837d65
Reena Koshta <reenakoshta@yahoo.com>
2021-12-17 11:47:04 +0100

    updated the file

--- app/steam_analytics.py
+++ app/steam_analytics.py
@@ -1,18 +1,18 @@
+from io import BytesIO
 import os
 import pandas as pd
 
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import send_file
 
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import inspect
 
-import matplotlib.pyplot as plt
-
 app = Flask(__name__)
-
+data_for_plt =[]
 
 @app.route("/")
 def index():
@@ -41,11 +41,19 @@ def index():
             axis=1,
             inplace=True,
         )
-
+    data_for_plt = data.head()
     return render_template(
-        "index.html", tables=[data.to_html(classes="data")], titles=data.columns.values
+        "index.html", tables=[data.to_html(classes="data")], titles=data.columns.values,dataframe = data
     )
 
-
+# @app.route('/fig/')
+# def fig():
+#       # plt.plot([1,2,3,4], [1,2,3,4])
+#       plt.pie(data_for_plt['is_free'])
+#       img = BytesIO()
+#       plt.savefig(img)
+#       img.seek(0)
+#       return send_file(img, mimetype='image/png')
+      
 if __name__ == "__main__":
     app.run(debug=True)
